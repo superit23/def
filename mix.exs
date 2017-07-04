210855e90d43a06cbcf10ad34ced8ce0b9b50f75
root <dankuscrankus@dankuscrank.us>
2017-07-04 02:35:19 -0500

    Works with Travis. Attempting to add CodeCov integration.

--- mix.exs
+++ mix.exs
@@ -21,6 +21,6 @@ defmodule Def.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:excoveralls, "~> 0.7.1"}]
   end
 end
